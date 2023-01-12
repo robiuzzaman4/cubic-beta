c2a55d69e641b47593375a50e49ba892736aad69
robiuzzaman4 <robiuzzaman4@gmail.com>
2023-01-12 08:00:47 +0600

    service section add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
+// SELECT MENU BUTTON & NAV LIST
 const menuBtn = document.querySelector("#menuBtn");
 const navList = document.querySelector("#navList");
 
+// ADD EVENT LISTENER ON MENU BUTTON & SHOWING RESPONSIVE NAV BAR
 menuBtn.addEventListener("click", () => {
     navList.classList.toggle("nav-active");
 });
